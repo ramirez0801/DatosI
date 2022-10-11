dfaef24bba0b7fccb8f49876dcfc305ad6807ef1
ramirez0801 <ramirez.erg@hotmail.com>
2022-10-10 22:30:42 -0500

    Verificar Funcion vaciar

--- Practica4/main.cpp
+++ Practica4/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//Clase Nodo
 class cNodo
 {
     private:
@@ -10,8 +11,8 @@ class cNodo
     public:
         cNodo();
         ~cNodo();
-        void agregar(float);
-        void eliminar();
+        void agregarSig(float);
+        float eliminarSig();
         friend class cLista;
 };
 
@@ -23,17 +24,29 @@ cNodo::~cNodo()
 {
 }
 
-void cNodo::agregar(float dato)
+void cNodo::agregarSig(float dato)
 {
     cNodo *pnn = new cNodo;
     pnn->dato = dato;
     pnn->sig = this->sig;
     pnn->ant = this;
+    this->sig->ant = pnn;
     this->sig = pnn;
-    cout<<"PNN: "<<pnn->dato<<endl;
 }
 
+float cNodo::eliminarSig()
+{
+    cNodo *pnn = new cNodo;
+    pnn = this->sig;
+    this->sig = pnn->sig;
+    pnn->sig->ant = this;
+    pnn->sig = NULL;
+    pnn->ant = NULL;
+    return pnn->dato;
+    ///delete pnn;
+}
 
+//Clase para lista
 class cLista
 {
     private:
@@ -47,6 +60,9 @@ class cLista
         int tamanio();
         bool vacio();
         void vaciar();
+        void mostrar();
+        float eliminarInicio();
+        float eliminarFinal();
 };
 
 cLista::cLista()
@@ -59,45 +75,113 @@ cLista::~cLista()
 {
 }
 
-void cLista::insertarInicio(float dato)
+void cLista::insertarInicio(float _dato)
 {
-    Inicio.agregar(dato);
+    Inicio.agregarSig(_dato);
+    //cNodo *pnn = new cNodo;
+    //cout<<"Inci1 "<<Inicio.sig->dato<<endl;
+    // pnn->sig = Inicio.sig;
+    // Inicio.sig->ant = pnn;
+    // Inicio.sig = pnn;
+    // pnn->ant = &Inicio;
+    // pnn->dato = _dato;
 }
 
-void cLista::insertarFinal(float dato)
+void cLista::insertarFinal(float _dato)
 {
-    cNodo *pnn = new cNodo;
-    pnn->dato = dato;
-    pnn->sig = &Final;
-    Final.sig = pnn;
+    Final.ant->agregarSig(_dato);
+    // cNodo *pnn = new cNodo;
+    // //cout<<"Final "<<Final.ant->dato;
+    // pnn->dato = _dato;
+    // pnn->sig = &Final;
+    // Final.ant->sig = pnn;
+    // Final.ant = pnn;
 }
 
 int cLista::tamanio()
 {
-    return 0;
+    int i;
+    cNodo *p = Inicio.sig;
+    //Recorre la lista y cuenta los nodos encontrados
+    for(i = 0; p != &Final; i++) 
+    {
+        p = p->sig;
+    }
+    return i;
 }
 
 bool cLista::vacio()
 {
-
+    //Si el nodo siguiente de la cabecera inicio es el nodo fina, la lista esta vacia
+    if(Inicio.sig == &Final) return true;
+    return false;
 }
 
 void cLista::vaciar()
 {
+    cNodo *aux = new cNodo;
+    cNodo *pn = new cNodo;
+    pn = Inicio.sig;
+    for(int i = 0; pn != &Final; i++)
+    {
+      aux = pn;
+      pn = pn->sig;
+      delete aux;
+    }
 
 }
 
+void cLista::mostrar()
+{
+    cNodo *pnn = new cNodo;
+    if(vacio())
+        cout<<"\n\t---Lista vacia---\n";
+    else
+    {
+        pnn = Inicio.sig;
+        for(int i = 1; pnn != &Final; i++)
+        {
+            cout<<"Dato["<<i<<"]: "<<pnn->dato<<endl;
+            pnn = pnn->sig;
+        }
+    }
+}
 
+float cLista::eliminarInicio()
+{
+    float dato;
+    dato = Inicio.eliminarSig();
+    return dato;
+}
+
+float cLista::eliminarFinal()
+{
+    float dato;
+    dato = Final.ant->ant->eliminarSig();
+    return dato;
+}
 
 int main(int argc, char const *argv[])
 {
-    cNodo *nodo = new cNodo;
-    cLista *pLista = new cLista;
-    
-    nodo->agregar(5);
-    nodo->agregar(10);
+    int num = 0;
+    float dato;
+    cLista *list = new cLista;
+    list->insertarInicio(5);
+    list->insertarInicio(6);
+    list->insertarFinal(10);
+    list->mostrar();
+    num = list->tamanio();
+    cout<<"Tamanio: "<<num<<'\n';
+    dato = list->eliminarInicio(),
+    cout<<"Dato eliminado inicio: "<<dato<<'\n';
+    dato = list->eliminarFinal(),
+    cout<<"Dato eliminado final: "<<dato<<'\n';
+    list->mostrar();
+    list->vaciar();
+    list->mostrar();
 
-    pLista->insertarInicio(8);
+    //cout<<"\n\t******\n\t1)Agregar al inicio\n\t2)Agregar al final\n\t3)Mostrar\n\t4)Tamanio\n\t5)Eliminar inicio\n\t6)Eliminar final\n\t7)Vaciar";    
+
     cout<<"Exito"<<endl;    
     return 0;
 }
